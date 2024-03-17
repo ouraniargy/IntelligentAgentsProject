28c992b2020862b8a050e593afcfa081f45516fc
Ourania Argyra <ouraniargy@gmail.com>
2024-03-17 16:04:20 +0200

    11/12
    Δημιουργία MainMenu. Τέλος 11/12. Το MainMenu περιέχει τη Start Και την Quit, παρέχοντας αρκετές βασικές λειτουργίες.

--- Assets/MainMenu.cs
+++ Assets/MainMenu.cs
@@ -6,11 +6,19 @@ using UnityEngine.UI;
 
 public class MainMenu : MonoBehaviour
 {
+    public static MainMenu Instance; //singleton
     public Slider progressBar;
     // Start is called before the first frame update
     void Start()
     {
-        
+        if (Instance == null)
+            Instance = this;
+        else
+        {
+            Destroy(gameObject);
+            return;
+        }
+        DontDestroyOnLoad(gameObject);
     }
 
     // Update is called once per frame
@@ -22,13 +30,15 @@ public class MainMenu : MonoBehaviour
     public void StartApp()
     {
         //TODO
+        
         Debug.Log("Start");
 
-        StartCoroutine(LoadYourAsyncScene());
+       StartCoroutine(LoadYourAsyncScene());
     }
 
     IEnumerator LoadYourAsyncScene()
     {
+        yield return new WaitForSeconds(5);
         AsyncOperation asyncLoad = SceneManager.LoadSceneAsync("MainMenu");
         asyncLoad.allowSceneActivation = false; 
         while (!asyncLoad.isDone)
@@ -37,6 +47,7 @@ public class MainMenu : MonoBehaviour
             yield return null;
             if (Input.GetKeyDown(KeyCode.E))
             {
+                //allow scene to be activated
                 asyncLoad.allowSceneActivation = true;
             }
         }
